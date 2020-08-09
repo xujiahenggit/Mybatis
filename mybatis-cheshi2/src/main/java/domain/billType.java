b5f3315dc1c9e9553ad7e21873e9e1961508ef7d
xujiahenggit <57058861+xujiahenggit@users.noreply.github.com>
2020-08-09 15:03:22 +0800

    ddd'd
    d'd

--- mybatis-cheshi2/src/main/java/domain/billType.java
+++ mybatis-cheshi2/src/main/java/domain/billType.java
@@ -27,10 +27,9 @@ public class billType {
     public void setName(String name) {
         this.name = name;
         System.out.println("操练与喜爱");
-        if("5"=="5"){
-            System.out.println("5==5"+"asd");
+        if("6"=="6"){
+            System.out.println("最后再试一次");
         }
-         System.out.println("我自己修改的代码");
     }
 
     @Override
